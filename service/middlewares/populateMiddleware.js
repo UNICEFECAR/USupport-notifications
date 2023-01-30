94d1bb0376197da9a423bcf48dbff5643d2c27b1
Ivan Ivanov <ivan.h.ivanov99@gmail.com>
2023-01-30 00:59:46 +0000

    refactor populateUser middleware

--- service/middlewares/populateMiddleware.js
+++ service/middlewares/populateMiddleware.js
@@ -1,16 +1,21 @@
-import { getUserByID } from "#queries/users";
+import fetch from "node-fetch";
+
+const USER_API_URL = process.env.USER_API_URL;
+const USER_API_LOCAL_HOST = "http://localhost:3010";
 
 export const populateUser = async (req, res, next) => {
-  const country = req.header("x-country-alpha-2");
   const user_id = req.header("x-user-id");
 
-  const user = await getUserByID(country, user_id)
-    .then((res) => res.rows[0])
-    .catch((err) => {
-      throw err;
-    });
+  const user = await fetch(`${USER_API_URL}/user/v1/user/${user_id}`, {
+    headers: {
+      ...req.headers,
+      host: USER_API_LOCAL_HOST,
+      "Content-type": "application/json",
+    },
+  })
+    .then((raw) => raw.json())
+    .catch(console.log);
 
   req.user = user;
-
   return next();
 };
