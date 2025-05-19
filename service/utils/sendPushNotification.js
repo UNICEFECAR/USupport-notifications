e06c91f4e9d671fe24dd1388cb79677aab4625f3
7Digit-Main <it-support@7digit.io>
2025-05-19 18:46:24 +0300

    Refactor: get the firebase credentials from the env

--- service/utils/sendPushNotification.js
+++ service/utils/sendPushNotification.js
@@ -1,9 +1,28 @@
-import { applicationDefault, initializeApp } from "firebase-admin/app";
+import { initializeApp, cert } from "firebase-admin/app";
 import { getMessaging } from "firebase-admin/messaging";
 import { chunkArray } from "./helperFunctions.js";
 
+const FIREBASE_PRIVATE_KEY_ID = process.env.FIREBASE_PRIVATE_KEY_ID;
+const FIREBASE_PRIVATE_KEY = process.env.FIREBASE_PRIVATE_KEY;
+const FIREBASE_CLIENT_EMAIL = process.env.FIREBASE_CLIENT_EMAIL;
+const FIREBASE_CLIENT_ID = process.env.FIREBASE_CLIENT_ID;
+const FIREBASE_CLIENT_X509_CERT_URL = process.env.FIREBASE_CLIENT_X509_CERT_URL;
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: "usupport-25db0",
+  private_key_id: FIREBASE_PRIVATE_KEY_ID,
+  private_key: FIREBASE_PRIVATE_KEY,
+  client_email: FIREBASE_CLIENT_EMAIL,
+  client_id: FIREBASE_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: FIREBASE_CLIENT_X509_CERT_URL,
+};
+
 initializeApp({
-  credential: applicationDefault(),
+  credential: cert(serviceAccount),
 });
 
 /**
