c321869dc8fb7c3fe7930db2d29df9ab3ac7418a
Stefan Dobrev <stefandobrev88@gmail.com>
2025-08-12 15:56:13 +0300

    Add: Armenian translations

---
+++ service/translations/am.js
@@ -0,0 +1,32 @@
+export default {
+  notification_not_found_error: 'Ծանուցումը չի գտնվել',
+  consultation_booking: 'Նոր խորհրդատվությունը պատվիրված է',
+  consultation_booking_message:
+    'Խորհրդատուի հետ {1} ձեր խորհրդատվությունը բարեհաջող վերապլանավորված է։',
+  consultation_suggestion: 'Նոր խորհրդատվության առաջարկ',
+  consultation_suggestion_message:
+    'Դուք խորհրդատուից {1} ստացել եք նոր խորհրդատվության առաջարկ։',
+  consultation_cancellation: 'Խորհրդատվությունը չեղարկված է',
+  consultation_cancellation_message:
+    'Դուք բարեհաջող չեղարկել եք խորհրդատվությունը խորհրդատուի {1} հետ։',
+  consultation_cancellation_by_provider_message:
+    'Խորհրդատուն{1} չեղարկել է ձեր առաջիկա խորհրդատվությունը',
+  consultation_suggestion_booking: 'Խորհրդատվության առաջարկն ընդունված է։',
+  consultation_suggestion_booking_message:
+    'Դուք ընդունել եք խորհրդատուի{1} կողմից առաջարկված խորհրդատվությունը։',
+  consultation_reschedule: 'Խորհրդատվությունը վերապլանավորված է։',
+  consultation_reschedule_message:
+    'Խորհրդատուի{1} հետ ձեր խորհրդատվությունը վերապլանավորված է',
+  consultation_reminder: 'Խորհրդատվության հիշեցում',
+  consultation_reminder_message:
+    'Խորհրդատուի{1} հետ ձեր խորհրդատվությունն սկսում է {2} րոպեից',
+  client: 'Հաճախորդ',
+  price: 'Արժեքը',
+  consultation_time: 'Խորհրդատվության ժամանակը',
+  campaign: 'Արշավը',
+  free: 'Անվճար',
+  scheduled_on: 'Պլանավորված է ամսի -ին',
+  consultation_started: 'Խորհրդատվությունն սկսված է',
+  consultation_started_message:
+    'Խորհրդատուի{1} հետ ձեր խորհրդատվությունն սկսված է։',
+};
